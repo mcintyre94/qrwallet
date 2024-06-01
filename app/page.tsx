8c4ee9e894496d9fa82aa62f6b9c941e8608053d
Callum <callum.mcintyre@anza.xyz>
2024-06-01 18:12:25 +0100

    Add the form to generate a pass

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,46 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import { Anchor, AppShell, Center, Container, Group, Image, Stack, Text, TextInput } from "@mantine/core";
+import { IconBrandGithub } from "@tabler/icons-react";
+import { useState } from "react";
 
 export default function Home() {
+  const [label, setLabel] = useState('')
+  const [toEncode, setToEncode] = useState('')
+  const active = label.length > 0 && toEncode.length > 0;
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <AppShell>
+      <AppShell.Main>
+        <Container p='xl'>
+          <Stack gap='xl'>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <form onSubmit={(e) => e.preventDefault()}>
+              <TextInput label="Label" onChange={(e) => setLabel(e.currentTarget.value)} />
+              <TextInput label="Data to encode" onChange={(e) => setToEncode(e.currentTarget.value)} />
+            </form>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+            {active ? (
+              <a href={`./generate?label=${encodeURIComponent(label)}&toEncode=${encodeURIComponent(toEncode)}`}>
+                <Image src='./apple-wallet-button.svg' w={120} />
+              </a>
+            ) : (
+              <Image src='./apple-wallet-button.svg' w={120} style={{ opacity: 0.1 }} />
+            )}
+          </Stack>
+        </Container>
+      </AppShell.Main>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <AppShell.Footer>
+        <Center>
+          <Anchor href='https://github.com/mcintyre94/pass-generator' underline='never' c='white'>
+            <Group>
+              <IconBrandGithub />
+              <Text>Source on Github</Text>
+            </Group>
+          </Anchor>
+        </Center>
+      </AppShell.Footer>
+    </AppShell>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ["@mantine/core"],
+  },
+};
 
 export default nextConfig;
